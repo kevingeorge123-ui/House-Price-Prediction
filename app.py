67d8109cc244cfddeac104b50c675dcf00db238b
kevingeorge123-ui <kevincgeorge6084@gmail.com>
2026-08-19 21:34:41 -0500

    Initialize Streamlit app for house price prediction

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Kevin C George - House Price Prediction")
+
+st.write("Welcome to my first Machine Learning Streamlit project!")
+
+st.write("This app will predict house prices.")
